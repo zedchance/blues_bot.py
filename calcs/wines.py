85d5e0fbcb5b1ec581e0c9d5f0414527ebbaa581
Zed Chance <zchance@sierracollege.edu>
2019-10-18 00:39:34 -0700

    Added wines calculator So far this just tells amount of wines needed to reach level 99

---
+++ calcs/wines.py
@@ -0,0 +1,24 @@
+# Wine calculator for cooking
+
+from helpers.hiscore import Hiscore
+from calcs.experience import LEVEL_99
+
+WINE_XP = 200
+WINE_XP_PER_INV = WINE_XP * 14
+
+class Wines():
+    """ Wine cooking calculator """
+    
+    def __init__(self, username):
+        self.username = username
+        user = Hiscore(username)
+        self.cooking_level = int(user.cooking_level)
+        self.cooking_xp = int(user.cooking_xp)
+    
+    def wines_to_level_99(self):
+        """ Returns the amount of wines needed to reach level 99 cooking """
+        return (LEVEL_99 - self.cooking_xp) // WINE_XP
+    
+    def invs_to_level_99(self):
+        """ Returns number of inventories to reach 99 cooking """
+        return (LEVEL_99 - self.cooking_xp) // WINE_XP_PER_INV
--- cogs/calculators.py
+++ cogs/calculators.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 
 from calcs.tasks import Tasks
+from calcs.wines import Wines
 
 class Calculators(commands.Cog):
     """ Commonly used calculators """
@@ -17,12 +18,25 @@ class Calculators(commands.Cog):
         """ Calculates estimated slayer tasks remaining """
         safe_username = ' '.join(username)
         user = Tasks(safe_username, num_of_tasks)
-        msg = f'**{user.slayer_level}** Slayer ({user.slayer_xp:,} xp) | {safe_username}'
+        msg = f'Slayer calculator\n**{user.slayer_level}** slayer ({user.slayer_xp:,} xp) | {safe_username}'
         avg = f'{user.avg_xp_per_task():,} average xp per task'
         to_level_up = f'{user.tasks_to_level_up()} tasks to level up ({user.xp_needed_to_level_up():,} xp)'
         to_level_99 = f'{user.tasks_to_level_99()} tasks to level 99'
         last_task = f'{user.estimated_total_tasks()} estimated total tasks'
         await ctx.send(f'{msg}\n{avg}\n{to_level_up}\n{to_level_99}\n{last_task}')
+        return
+
+    @commands.command(name='wines',
+        description='Wine cooking calculator',
+        aliases=['wine'],
+        case_insensitive=True)
+    async def wines_command(self, ctx, *username):
+        """ Calculates wines needed to level 99 """
+        safe_username = ' '.join(username)
+        user = Wines(safe_username)
+        msg = f'Wine calculator\n**{user.cooking_level}** cooking ({user.cooking_xp:,} xp) | {safe_username}'
+        remaining = f'{user.wines_to_level_99():,} wines needed to reach level 99 ({user.invs_to_level_99():,} inventories)'
+        await ctx.send(f'{msg}\n{remaining}')
         return
 
 # Cog setup
