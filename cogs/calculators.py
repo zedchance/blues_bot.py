d4a98f769fe61c6559d93213defc86e21ea9469b
Zed Chance <zchance@sierracollege.edu>
2019-10-17 23:40:24 -0700

    Swapped parameters on tasks to compensate for users with spaces in their names

--- cogs/calculators.py
+++ cogs/calculators.py
@@ -12,11 +12,12 @@ class Calculators(commands.Cog):
         description='Slayer task calculator',
         aliases=['task'],
         case_insensitive=True)
-    async def tasks_command(self, ctx, username, tasks):
+    async def tasks_command(self, ctx, num_of_tasks, *username):
         """ Calculates estimated tasks remaining """
-        user = Tasks(username, tasks)
-        msg = f'{user.slayer_level} Slayer ({user.slayer_xp:,} xp) | {username}'
-        avg = f'{user.avg_xp_per_task()} average xp per task'
+        safe_username = ' '.join(username)
+        user = Tasks(safe_username, num_of_tasks)
+        msg = f'{user.slayer_level} Slayer ({user.slayer_xp:,} xp) | {safe_username}'
+        avg = f'{user.avg_xp_per_task():,} average xp per task'
         to_level_up = f'{user.tasks_to_level_up()} tasks to level up'
         to_level_99 = f'{user.tasks_to_level_99()} tasks to level 99'
         last_task = f'{user.estimated_total_tasks()} estimated total tasks'
