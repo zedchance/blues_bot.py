0653dfd1a260c3e33cc490361af13ffb093b0902
Zed Chance <zchance@sierracollege.edu>
2019-10-19 22:30:50 -0700

    Zeah runecraft calculator rounds up and shows integers

--- cogs/calculators.py
+++ cogs/calculators.py
@@ -55,16 +55,16 @@ class Calculators(commands.Cog):
             await ctx.send(f'{ctx.message.author.mention}\n{msg}\nYou must have at least 77 runecraft for blood runes and 90 for soul runes')
         elif (user.runecraft_level < 90):
             xp = f'{user.xp_needed_to_level_up():,} xp to level {user.runecraft_level + 1}'
-            bloods_needed = f'{user.bloods_to_level_up():,.2f} bloods to level up (~ {user.blood_trips_to_level_up():,.1f} trips)'
-            bloods_to_99 = f'{user.bloods_to_level_99():,.2f} bloods to level 99 (~ {user.blood_trips_to_level_99():,.1f} trips)'
+            bloods_needed = f'{user.bloods_to_level_up() + 1:,.0f} bloods to level up (~ {user.blood_trips_to_level_up() + 1:,.0f} trips)'
+            bloods_to_99 = f'{user.bloods_to_level_99() + 1:,.0f} bloods to level 99 (~ {user.blood_trips_to_level_99() + 1:,.0f} trips)'
             await ctx.send(f'{ctx.message.author.mention}\n{msg}\n{xp}\n{bloods_needed}\n{bloods_to_99}')
         else:
             xp = f'{user.xp_needed_to_level_up():,} xp to level {user.runecraft_level + 1}'
-            bloods_needed = f'{user.bloods_to_level_up():,.2f} bloods to level up (~ {user.blood_trips_to_level_up():,.1f} trips)'
-            souls_needed = f'{user.souls_to_level_up():,.2f} souls to level up (~ {user.soul_trips_to_level_up():,.1f} trips)'
+            bloods_needed = f'{user.bloods_to_level_up() + 1:,.0f} bloods to level up (~ {user.blood_trips_to_level_up() + 1:,.0f} trips)'
+            souls_needed = f'{user.souls_to_level_up() + 1:,.0f} souls to level up (~ {user.soul_trips_to_level_up() + 1:,.0f} trips)'
             if (user.runecraft_level < 99):
-                bloods_to_99 = f'{user.bloods_to_level_99():,.2f} bloods to level 99 (~ {user.blood_trips_to_level_99() + 1:,.0f} trips)'
-                souls_to_99 = f'{user.souls_to_level_99():,.2f} souls to level 99 (~ {user.soul_trips_to_level_99() + 1:,.0f} trips)'
+                bloods_to_99 = f'{user.bloods_to_level_99() + 1:,.0f} bloods to level 99 (~ {user.blood_trips_to_level_99() + 1:,.0f} trips)'
+                souls_to_99 = f'{user.souls_to_level_99() + 1:,.0f} souls to level 99 (~ {user.soul_trips_to_level_99() + 1:,.0f} trips)'
                 await ctx.send(f'{ctx.message.author.mention}\n{msg}\n{xp}\n{bloods_needed}\n{bloods_to_99}\n{souls_needed}\n{souls_to_99}')
             else:
                 await ctx.send(f'{ctx.message.author.mention}\n{msg}\n{xp}\n{bloods_needed}\n{souls_needed}')
