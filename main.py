338c87a1fd89cffb5efa62810faad9a9c4b9317a
Zed Chance <zchance@sierracollege.edu>
2019-10-20 18:48:53 -0700

    Moved basic commands to "no category"
    Version command is now hidden
    
    Reload command can be used to reload cogs while bot is online
    (I want this to only work if I use the command)
    
    Removed basic category from cogs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 from helpers.api_key import discord_key, owner_id
-from helpers.descriptions import bot_description
+from helpers.descriptions import bot_description, version_number
 
 def get_prefix(client, message):
     prefixes = ['!blue ', '!b ']
@@ -15,7 +15,7 @@ bot = commands.Bot(command_prefix=get_prefix,
         owner_id=owner_id,
         case_insensitive=True)
 
-cogs = ['cogs.basic', 'cogs.links', 'cogs.levels', 'cogs.calculators', 'cogs.scores']
+cogs = ['cogs.links', 'cogs.levels', 'cogs.calculators', 'cogs.scores']
 
 @bot.event
 async def on_ready():
@@ -29,5 +29,38 @@ async def on_ready():
 # @bot.event
 # async def on_command_error(ctx, error):
 
+@bot.command(name='reload',
+    description='Reloads bot',
+    aliases=['-r'],
+    hidden=True,
+    case_insensitive=True)
+async def reload(ctx):
+    """ Reloads cogs while bot is still online """
+    for cog in cogs:
+        bot.unload_extension(cog)
+    for cog in cogs:
+        print("Reloading", cog)
+        bot.load_extension(cog)
+    await ctx.send("Cogs reloaded")
+
+@bot.command(name='version',
+    description='Bot version',
+    aliases=['--version', '-v'],
+    hidden=True,
+    case_insensitive=True)
+async def version_command(ctx):
+    """ Shows bot version number """
+    await ctx.send(f'{ctx.message.author.mention}\n**!blue** *version {version_number}*\nRecent changes here:\nhttps://github.com/zedchance/blues_bot.py/commits/master')
+    return
+    
+@bot.command(name='bug',
+    description='Provides a link to the bug/issue page',
+    aliases=['issue'],
+    case_insensitive=True)
+async def bug_command(ctx, *message):
+    """ Links to bug/issue page """
+    msg = ' '.join(message)
+    await ctx.send(f'{ctx.message.author.mention}\nPlease report all issues and bugs here:\nhttps://github.com/zedchance/blues_bot.py/issues')
+    return
 
 bot.run(discord_key, bot=True, reconnect=True)
