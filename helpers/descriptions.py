c9c8aa935013765e5d64666b70e944418e538c29
Zed Chance <zedchance@gmail.com>
2019-10-21 22:32:56 -0700

    Updated version number to 0.5

--- helpers/descriptions.py
+++ helpers/descriptions.py
@@ -1,7 +1,8 @@
 # File to hold longer descriptions
 
 bot_description = '!blue Old School Runescape bot'
-version_number = '0.4 (20191018)'
+# version_number = '0.4 (20191018)'
+version_number = '0.5 (20191021)'
 wrong_message = '''
 ```
  ______________________ 
