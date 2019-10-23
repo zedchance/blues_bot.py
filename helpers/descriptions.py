f764bbc3af40bfc4e4661875b64ac9162dfa86dd
Zed Chance <zchance@sierracollege.edu>
2019-10-23 08:52:37 -0700

    Descriptions now has fields for embed_help

--- helpers/descriptions.py
+++ helpers/descriptions.py
@@ -1,5 +1,8 @@
 # File to hold longer descriptions
 
+prefix = '!b '
+bot_title = '!blue'
+help_footer = 'Use `!blue help [command]` or `!blue help [category]` for more information'
 bot_description = '!blue Old School Runescape bot'
 # version_number = '0.4 (20191018)'
 version_number = '0.5 (20191021)'
