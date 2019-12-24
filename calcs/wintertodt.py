653434c6a9c2807a0e2f271b75b14781be5509d4
Zed Chance <zchance@sierracollege.edu>
2019-12-23 22:24:35 -0800

    Corrected kills_to_level_up in wt command
    Removed redundant int parse

--- calcs/wintertodt.py
+++ calcs/wintertodt.py
@@ -1,4 +1,4 @@
-from calcs.experience import LEVEL_99, xp_to_next_level
+from calcs.experience import LEVEL_99, xp_to_next_level, level_to_xp
 from helpers.hiscore import Hiscore
 
 class Wintertodt(Hiscore):
@@ -6,17 +6,17 @@ class Wintertodt(Hiscore):
 
     # Average xp per kill
     def average(self):
-        return int(self.firemaking_xp) // int(self.kc_wintertodt)
+        return self.firemaking_xp // self.kc_wintertodt
 
     # Estimated kills until level up
     def kills_to_level_up(self):
-        remaining = xp_to_next_level(int(self.firemaking_level))
+        remaining = (level_to_xp(self.firemaking_level + 1)) - self.firemaking_xp
         return remaining // self.average()
 
     # Estimated kills remaining until level 99
     def kills_to_level_99(self):
-        return (LEVEL_99 - int(self.firemaking_xp)) // self.average()
+        return (LEVEL_99 - self.firemaking_xp) // self.average()
 
     # Estimate total kills
     def estimated_total_kills(self):
-        return int(self.kc_wintertodt) + self.kills_to_level_99()
+        return self.kc_wintertodt + self.kills_to_level_99()
