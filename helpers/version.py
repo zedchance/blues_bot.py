9ba7dcebc3bc9fbab08d1d9bd82203b77567e9bc
Zed Chance <zchance@sierracollege.edu>
2019-12-30 14:55:10 -0800

    Version command now uses git describe
    Shows the describe of the last origin/master commit

---
+++ helpers/version.py
@@ -0,0 +1,9 @@
+# Version command helper
+
+def get_version():
+    """ Opens version file and returns it as a string """
+    file = open("assets/version", "r")
+    ret = ''
+    for line in file:
+        ret += line
+    return ret
